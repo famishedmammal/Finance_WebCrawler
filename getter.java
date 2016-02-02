709f170285afc610baa987e1a0c34ee283d79527
Harrison Walters <famishedmammal@gmail.com>
2016-02-02 12:51:13 -0800

    added comment blocks

--- getter.java
+++ getter.java
@@ -16,55 +16,67 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
 public class getter {
-	
-	public static String companyName(String company) throws Exception {
-		
-		URL getURL = new URL("http://download.finance.yahoo.com/d/quotes.csv?s="+company+"&f=n&e=.csv");
+
+/*--------------------- companyName() routine--------------------------*
+ * given a ticker symbol, extract the company name
+ *---------------------------------------------------------------------------*/
+
+public static String companyName(String company) throws Exception {
+	URL getURL = new URL("http://download.finance.yahoo.com/d/quotes.csv?s="+company+"&f=n&e=.csv");
         BufferedReader in = new BufferedReader(new InputStreamReader(getURL.openStream()));
         String inputLine = in.readLine();
         in.close();
         
         return inputLine;
-		
-	}
+}
 	
-	public static String [] generalData(String company) throws Exception {
+/*--------------------- generalData() routine--------------------------*
+ * givena ticker symbol, extract information from yahoo finance and return it in CSV format
+ *---------------------------------------------------------------------------*/
+	
+public static String [] generalData(String company) throws Exception {
 		
-		URL getURL = new URL("http://download.finance.yahoo.com/d/quotes.csv?s="+company+"&f=aa2a5bb4b6cp2dee7e8e9gf6hjj1j4j5j6kk4k5mm3m4m5m6m7m8opp5rr5s6s7t8vwy&e=.csv");
+	URL getURL = new URL("http://download.finance.yahoo.com/d/quotes.csv?s="+company+"&f=aa2a5bb4b6cp2dee7e8e9gf6hjj1j4j5j6kk4k5mm3m4m5m6m7m8opp5rr5s6s7t8vwy&e=.csv");
         BufferedReader in = new BufferedReader(new InputStreamReader(getURL.openStream()));
         String inputLine = in.readLine();
         in.close();
         
         return inputLine.split(",");
-        
-	}
+}
+
+/*--------------------- industry() routine--------------------------*
+ * given a ticker symbol, extract industry type and related industries.
+ *---------------------------------------------------------------------------*/
 	
-	public static String industry(String company) {
+public static String industry(String company) {
 		
-		Document doc = null;
-		try {
-			doc = Jsoup.connect("https://finance.yahoo.com/q/in?s="+company+"+Industry").get();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		return doc.select("#yfncsumtab > tbody > tr:nth-child(2) > td:nth-child(1) > table.yfnc_datamodoutline1 > tbody > tr > td > table > tbody > tr:nth-child(2) > td > a").text();
-    	
+	Document doc = null;
+	try {
+		doc = Jsoup.connect("https://finance.yahoo.com/q/in?s="+company+"+Industry").get();
+	} catch (IOException e) {
+		e.printStackTrace();
 	}
+		
+	return doc.select("#yfncsumtab > tbody > tr:nth-child(2) > td:nth-child(1) > table.yfnc_datamodoutline1 > tbody > tr > td > table > tbody > tr:nth-child(2) > td > a").text();
+}
+
+/*--------------------- competition() routine--------------------------*
+ * given a ticker symbol, extract list of top competitors
+ *---------------------------------------------------------------------------*/
 	
-	public static String [] competition(String company) {
+public static String [] competition(String company) {
 		
-		Document doc = null;
-		try {
-			doc = Jsoup.connect("https://ca.finance.yahoo.com/q/co?s="+company).get();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+	Document doc = null;
+	try {
+		doc = Jsoup.connect("https://ca.finance.yahoo.com/q/co?s="+company).get();
+	} catch (IOException e) {
+		e.printStackTrace();
+	}
 		
-		String [] listData = new String[3];
+	String [] listData = new String[3];
 		
-		String str;
-		str = doc.select("#yfncsumtab > tbody > tr:nth-child(2) > td:nth-child(1) > table.yfnc_datamodoutline1 > tbody > tr > td > table > tbody > tr:nth-child(1) > th:nth-child(3) > a").text();
+	String str;
+	str = doc.select("#yfncsumtab > tbody > tr:nth-child(2) > td:nth-child(1) > table.yfnc_datamodoutline1 > tbody > tr > td > table > tbody > tr:nth-child(1) > th:nth-child(3) > a").text();
     	if (!str.equals(""))
     		listData[0] = str; else
     			listData[0] = "N/A";
